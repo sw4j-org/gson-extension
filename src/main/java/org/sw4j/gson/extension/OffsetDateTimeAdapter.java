5d170d5bb4c8793b950ad6531dd058ac83e6cd04
Uwe Plonus <u.plonus@gmail.com>
2017-12-23 20:01:52 +0100

    Changed code to adhere to checkstyle rules.

--- src/main/java/org/sw4j/gson/extension/OffsetDateTimeAdapter.java
+++ src/main/java/org/sw4j/gson/extension/OffsetDateTimeAdapter.java
@@ -15,10 +15,20 @@ import java.time.format.DateTimeFormatter;
  */
 public class OffsetDateTimeAdapter extends TypeAdapter<OffsetDateTime> {
 
+    /** The default constructor. */
     public OffsetDateTimeAdapter() { }
 
+    /**
+     * <p>
+     * Writes out the given OffsetDateTime as ISO formatted string.
+     * </p>
+     *
+     * @param out the JsonWriter where the data should be written to.
+     * @param value the OffsetDateTime to write.
+     * @throws IOException is the writing fails.
+     */
     @Override
-    public void write(JsonWriter out, OffsetDateTime value) throws IOException {
+    public void write(final JsonWriter out, final OffsetDateTime value) throws IOException {
         if (value == null) {
             out.nullValue();
         } else {
@@ -26,8 +36,17 @@ public class OffsetDateTimeAdapter extends TypeAdapter<OffsetDateTime> {
         }
     }
 
+    /**
+     * <p>
+     * Reads in a value formatted in ISO format as OffsetDateTime.
+     * </p>
+     *
+     * @param in the JsonReader to read from.
+     * @return the parsed OffsetDateTime.
+     * @throws IOException if the reading fails.
+     */
     @Override
-    public OffsetDateTime read(JsonReader in) throws IOException {
+    public OffsetDateTime read(final JsonReader in) throws IOException {
         OffsetDateTime result = null;
         if (in.peek() == JsonToken.NULL) {
             in.nextNull();
